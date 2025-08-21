ed25314c027baba3a9d963d33e023b83716f8706
Michael Trommer <mictro@gmail.com>
2025-08-21 17:22:54 +0200

    --xff default added

--- lib/ytdltt/ytdltt.rb
+++ lib/ytdltt/ytdltt.rb
@@ -94,8 +94,10 @@ module YTDLTT
 
     def ytdlp_default_arguments(*args)
       [ '-o', title_template, '--restrict-filenames', '--print', 'after_move:filepath',
-        '--fragment-retries', "6", "--retries", "3",
-        '--no-playlist', '--no-post-overwrites', '--no-mtime', '--no-write-comments' ] + args
+        '--fragment-retries', "3", "--retries", "3",
+        '--no-playlist', '--no-post-overwrites', '--no-mtime', '--no-write-comments',
+        '--xff', 'default'
+      ] + args
     end
 
     def full_arguments
